30ebb58fc627d15d6efa893400d53ceebe23552a
Chrys <bagwellchristopher@hotmail.co.uk>
2025-10-14 21:54:25 +0100

    Added paused track display

--- get_mpris_media_playing.sh
+++ get_mpris_media_playing.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 
+get_metadata() {
+  echo $(playerctl -p "$1" metadata --format "{{artist}} - {{title}}")
+}
+
 # assuming mapfile
 mapfile -t players < <( playerctl -l ) || exit 1
 mapfile -t statuses < <( playerctl -a status ) || exit 1
 
-for index in ${!statuses[@]}; do
+# players that are currently "Playing" take priority
+for index in "${!statuses[@]}"; do
   if [ "${statuses[$index]}" == "Playing" ]; then
-    metadata=$(playerctl -p "${players[$index]}" metadata --format "{{artist}} - {{title}}")
+    player="${players[$index]}"
+    metadata=$(get_metadata "$player")
+    prefix=""
     break
   fi
 done
 
-echo "$metadata"
+# if nothing is playing, then take the first player that is paused
+if [[ ! "$metadata" ]]; then
+  if [ "${statuses[0]}" == "Paused" ]; then
+    player="${players[0]}"
+    metadata=$(get_metadata "$player")
+    prefix="(Paused) "
+  fi
+fi
+
+echo "${prefix}${metadata}"
